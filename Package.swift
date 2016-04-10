84acfe89e3afdfc1a43c5ba4d41d07e0b2aee291
Kyle Jessup <badthing@gmail.com>
2016-04-09 22:51:11 -0400

    Subfolders

---
+++ Package.swift
@@ -0,0 +1,30 @@
+//
+//  Package.swift
+//  PerfectExamples
+//
+//  Created by Kyle Jessup on 3/22/16.
+//	Copyright (C) 2016 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PackageDescription
+
+let package = Package(
+	name: "SQLite3",
+	targets: [
+		
+	],
+	dependencies: [
+		
+	]
+)
