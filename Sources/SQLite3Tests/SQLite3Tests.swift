aa9095c4ae25acde8d64311a93d502dad08ef30b
Kyle Jessup <badthing@gmail.com>
2016-04-11 18:57:23 -0400

    Moving Xcode project back in

---
+++ Sources/SQLite3Tests/SQLite3Tests.swift
@@ -0,0 +1,25 @@
+//
+//  Perfect_SQLite3Tests.swift
+//  Perfect-SQLite3Tests
+//
+//  Created by Kyle Jessup on 2016-04-09.
+//  Copyright © 2016 PerfectlySoft. All rights reserved.
+//
+
+import XCTest
+@testable import SQLite
+
+class Perfect_SQLite3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
